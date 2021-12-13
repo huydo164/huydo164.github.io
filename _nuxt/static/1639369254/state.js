ba7ec7e0bc542583b0fb0a0ac6b89a6c57e7d0c2
huydo164 <67752571+huydo164@users.noreply.github.com>
2021-12-13 11:22:08 +0700

    Add files via upload

---
+++ _nuxt/static/1639369254/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1639369254",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"120a15e2"}}}}(null,"\u002F"));
